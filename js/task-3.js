83f6ba2102ac15c9d0abe9c0e1facc5f7b3b6f31
Roman <motkoroma2702@gmail.com>
2025-05-15 20:57:26 +0200

    done tasks

--- js/task-3.js
+++ js/task-3.js
@@ -1,2 +1,21 @@
 'use strict';
+function checkForSpam(message) {
+    const cancelWord1 = "spam";
+    const cancelWord2 = "sale";
+    let messageCheck = message.toLowerCase();
+    if (messageCheck.includes(cancelWord1) || messageCheck.includes(cancelWord2)) {
+        return true;
+    } else {
+        return false;
+    }
+    
+}
 
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
